3cff7eac4e53e80f8be8a0deecb1a7501bd3111d
Levi Kane <oneleggedlevi09@gmail.com>
2021-01-05 15:11:30 -0600

    responsive video layouts
    finalluy figured out the responsive video layout thing

--- random-website/src/components/VideoLayouts.vue
+++ random-website/src/components/VideoLayouts.vue
@@ -1,17 +1,17 @@
 <template>
     <v-container>
         <v-layout row wrap mb-10>
-            <v-col cols="12">
+            <v-col cols="12" class="order-0 order-sm-0">
                 <div class="text-h3"><strong>Cool Vids</strong></div>
             </v-col>
-            <v-col cols="12" lg="6" xl="6"
+            <v-col cols="12" class="order-1 order-sm-1" md="6"
                 ><v-icon large color="black">mdi-face-outline</v-icon
                 ><br /><span class="text-h6 font-weight-bold"
                     >First things First</span
                 ><br />
                 1 colum 2 this is the first things first column</v-col
             >
-            <v-col cols="12" lg="6" xl="6"
+            <v-col cols="12" class="order-2 order-sm-2" md="6"
                 ><iframe
                     :width="`${video.width}`"
                     :height="`${video.height}`"
@@ -21,7 +21,7 @@
                     allowfullscreen
                 ></iframe
             ></v-col>
-            <v-col cols="12" lg="6" xl="6"
+            <v-col cols="12" class="order-4 order-md-3" md="6"
                 ><iframe
                     :width="`${video.width}`"
                     :height="`${video.height}`"
@@ -31,14 +31,14 @@
                     allowfullscreen
                 ></iframe
             ></v-col>
-            <v-col cols="12" lg="6" xl="6"
+            <v-col cols="12" class="order-3 order-md-4" md="6"
                 ><v-icon large color="black">mdi-ear-hearing</v-icon><br /><span
                     class="text-h6 font-weight-bold"
                     >I Hear Everything</span
                 ><br />This is the section where I talk about how I can hear
                 everything all of the time. The truth is that I cant!
             </v-col>
-            <v-col cols="12" lg="6" xl="6"
+            <v-col cols="12" class="order-5 order-sm-5" md="6"
                 ><v-icon large color="black"
                     >mdi-comment-processing-outline</v-icon
                 ><br /><span class="text-h6 font-weight-bold"
@@ -47,7 +47,7 @@
                 interactions with a real person like MYSELF! How cool is
                 that?!</v-col
             >
-            <v-col cols="12" lg="6" xl="6"
+            <v-col cols="12" class="order-6 order-sm-6" md="6"
                 ><iframe
                     :width="`${video.width}`"
                     :height="`${video.height}`"

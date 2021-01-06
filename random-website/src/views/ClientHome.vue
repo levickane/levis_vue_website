3ed90fa21b1495c4beb2cde75f8fcacb1ddcaa65
Levi Kane <oneleggedlevi09@gmail.com>
2021-01-06 08:28:17 -0600

    client home
    added the layout for pitch deck and capital raise plan. still need to figure out how to download files on click

--- random-website/src/views/ClientHome.vue
+++ random-website/src/views/ClientHome.vue
@@ -5,6 +5,30 @@
                 Your Profile
             </div>
         </v-layout>
+        <v-layout row wrap>
+            <v-col cols="12" sm="3" class="mb-10 ml-5"
+                ><v-divider></v-divider
+                ><v-img
+                    height="100"
+                    width="200"
+                    :src="`${pictures.picture2}`"
+                ></v-img
+                ><br /><span class="font-weight-bold" style="font-size: 1.5em"
+                    >Pitch Deck</span
+                ><v-icon large>mdi-arrow-collapse-down</v-icon></v-col
+            >
+            <v-col cols="12" sm="3" class="mb-10 ml-5"
+                ><v-divider></v-divider
+                ><v-img
+                    height="100"
+                    width="200"
+                    :src="`${pictures.picture1}`"
+                ></v-img
+                ><br /><span class="font-weight-bold" style="font-size: 1.5em"
+                    >Capital Raise Plan</span
+                ><v-icon large>mdi-arrow-collapse-down</v-icon></v-col
+            >
+        </v-layout>
         <have-questions></have-questions>
     </v-container>
 </template>
@@ -14,6 +38,16 @@ import HaveQuestions from '../components/HaveQuestions.vue';
 export default {
     components: { HaveQuestions },
     name: 'ClientHome',
+    data() {
+        return {
+            pictures: {
+                picture1:
+                    'https://toppng.com/uploads/preview/rolls-and-stacks-of-money-stacks-of-money-11562998046h21kuiihqr.png',
+                picture2:
+                    'https://toppng.com/uploads/preview/computer-icon-vector-png-desktop-computer-11563207085f1wwos2r52.png',
+            },
+        };
+    },
 };
 </script>
 

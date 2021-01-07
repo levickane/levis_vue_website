45c7683ca671c8df5d5008fce2820519b27e2958
Levi Kane <oneleggedlevi09@gmail.com>
2021-01-07 14:01:09 -0600

    no dist
    asdf

--- dist/js/chunk-2d0d040b.d575f0fe.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d040b"],{"66d9":function(a,n,e){"use strict";e.r(n);var t=function(){var a=this,n=a.$createElement,e=a._self._c||n;return e("v-container",[e("v-layout",[e("h1",[a._v("This is a page about Testimonials")])])],1)},i=[],o={name:"Testimonials"},s=o,u=e("2877"),c=e("6544"),l=e.n(c),r=e("a523"),d=e("a722"),p=Object(u["a"])(s,t,i,!1,null,"b55d1a90",null);n["default"]=p.exports;l()(p,{VContainer:r["a"],VLayout:d["a"]})}}]);
-//# sourceMappingURL=chunk-2d0d040b.d575f0fe.js.map

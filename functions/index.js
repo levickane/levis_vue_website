1effe6ff4a6045d655aa9eb83f0ace80a65a4f37
Levi Kane <oneleggedlevi09@gmail.com>
2021-01-11 15:54:15 -0600

    messing around with functions folder

--- functions/index.js
+++ functions/index.js
@@ -51,7 +51,7 @@ function sendEmail(html) {
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//     functions.logger.info("Hello logs!", { structuredData: true });
-//     response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+});

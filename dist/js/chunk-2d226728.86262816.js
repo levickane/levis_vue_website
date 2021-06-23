1cd591f807d0ac4bbfe449387c6ed6013f91f44f
Levi Kane <oneleggedlevi09@gmail.com>
2021-06-23 12:18:53 -0500

    added dist folder

---
+++ dist/js/chunk-2d226728.86262816.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d226728"],{e964:function(a,e,t){"use strict";t.r(e);var n=function(){var a=this,e=a.$createElement,t=a._self._c||e;return t("v-container",[t("v-layout",[t("h1",[a._v("This is a page about Real Estate")])])],1)},o=[],s={name:"RealEstate"},u=s,c=t("2877"),i=t("6544"),l=t.n(i),r=t("a523"),p=t("a722"),f=Object(c["a"])(u,n,o,!1,null,"d2ff8c30",null);e["default"]=f.exports;l()(f,{VContainer:r["a"],VLayout:p["a"]})}}]);
+//# sourceMappingURL=chunk-2d226728.86262816.js.map

4074ddbf537106f50488a291f6407f9d31280f8d
Levi Kane <oneleggedlevi09@gmail.com>
2021-06-24 09:14:32 -0500

    removed the dist folder from the git ignore

---
+++ dist/js/chunk-2d226728.226d848c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d226728"],{e964:function(a,e,t){"use strict";t.r(e);var n=function(){var a=this,e=a.$createElement,t=a._self._c||e;return t("v-container",[t("v-layout",[t("h1",[a._v("This is a page about Real Estate")])])],1)},o=[],s={name:"RealEstate"},u=s,c=t("2877"),i=t("6544"),l=t.n(i),r=t("a523"),p=t("a722"),w=Object(c["a"])(u,n,o,!1,null,"7e3e1976",null);e["default"]=w.exports;l()(w,{VContainer:r["a"],VLayout:p["a"]})}}]);
+//# sourceMappingURL=chunk-2d226728.226d848c.js.map

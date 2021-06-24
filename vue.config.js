6de09484667cb516921c1b1eb7d8026cd80dd441
Levi Kane <oneleggedlevi09@gmail.com>
2021-06-24 10:30:29 -0500

    changed public path to be very basic

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
     transpileDependencies: ['vuetify'],
-    publicPath:
-        process.env.NODE_ENV === 'production' ? '/levis_vue_website' : '/'
+    publicPath: '/levis_vue_website/dist/'
 };

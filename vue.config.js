9f9a244f8cc68076192381055ad0a78874bd3d2b
Levi Kane <oneleggedlevi09@gmail.com>
2021-06-24 10:25:01 -0500

    vue.config public path was changed back to normal without the dist path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     transpileDependencies: ['vuetify'],
     publicPath:
-        process.env.NODE_ENV === 'production' ? '/levis_vue_website/dist' : '/'
+        process.env.NODE_ENV === 'production' ? '/levis_vue_website' : '/'
 };
